f15b00b762278283f7e6c884fa0c61771bbdf553
kamildlugajczyk <kam.dlugajczyk@gmail.com>
2020-04-08 09:34:16 +0200

    Add excersise 2

--- Lab 5 - STL Iterators and Algorithms/Lab 5 - STL Iterators and Algorithms/main.cpp
+++ Lab 5 - STL Iterators and Algorithms/Lab 5 - STL Iterators and Algorithms/main.cpp
@@ -8,6 +8,8 @@
 
 int main()
 {
+	//------------------------ zadanie 1 ------------------------//
+
 	std::vector<int> intVector;
 	std::vector<int> copy_intVector(20);
 	srand(time(NULL));
@@ -26,6 +28,23 @@ int main()
 
 	std::cout << "\nMax: " << *max_element(copy_intVector.begin(), copy_intVector.end()) << std::endl;
 	std::cout << "Max: " << *min_element(copy_intVector.begin(), copy_intVector.end()) << std::endl;
+
+	//------------------------ zadanie 2 ------------------------//
+
+	generate(intVector.begin(), intVector.end(), [n = 1]() mutable { return n++; });
+
+	std::cout << "\n\nRosnaco: ";
+	for_each(intVector.begin(), intVector.end(), [&](int & value) {std::cout << value << ", "; });
+
+	reverse(intVector.begin(), intVector.end());
+
+	std::cout << "\nMalejaco: ";
+	for_each(intVector.begin(), intVector.end(), [&](int & value) {std::cout << value << ", "; });
+
+	if (find(intVector.begin(), intVector.end(), 7) != intVector.end())
+		std::cout << "\nZnaleziono szukana 7!" << std::endl;
+	else
+		std::cout << "\nNie znaleziono szukanej 7!" << std::endl;
 
 	std::getchar();
 	return 0;
