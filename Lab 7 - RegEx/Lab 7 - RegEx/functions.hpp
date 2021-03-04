08e3f4e77dece36a0d4fcb3b12e73cf7a3cbe5ea
kamildlugajczyk <kam.dlugajczyk@gmail.com>
2021-03-04 17:47:25 +0100

    Delete laboratory mini projects

--- Lab 7 - RegEx/Lab 7 - RegEx/functions.hpp
+++
@@ -1,9 +0,0 @@
-#pragma once
-#include <iostream>
-#include <regex>
-
-bool isLoginValid(std::string login);
-
-bool isEmailValid(std::string email);
-
-void showMatches(std::string string, std::regex regex);
